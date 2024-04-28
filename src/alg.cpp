f486990d182d96b0751619900e8a1755070fad39
Rmrvn <160429541+Rmrvn@users.noreply.github.com>
2024-04-28 19:48:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,32 @@
 // Copyright 2021 NNTU-CS
+int cbinsearch(int* arr, int size, int value) {
+  int c = 0;
+  int lt = 0;
+  int rt = size - 1;
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    while (lt <= rt) {
+        int mid = lt + (rt - lt) / 2;
+        if (arr[mid] == value) {
+ c++;
+            int i = mid - 1;
+            while (i >= 0 && arr[i] == value) {
+ c++;
+ i--;
+            }
+ i = mid + 1;
+            while (i < size && arr[i] == value) {
+ c++;
+ i++;
+            }
+            return c;
+ } else if (arr[mid] < value) {
+            lt = mid + 1;
+        } else {
+            rt = mid - 1;
+        }
+    }
+    if (c == 0) {
+        return 0;
+    }
+    return c;
 }
